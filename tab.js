43afff2c324409f1e54bdb8d6d891788afe4fcd1
i015049 <76279350+i015049@users.noreply.github.com>
2020-12-26 08:22:48 +0900

    initial comit 20201226

---
+++ tab.js
@@ -0,0 +1,13 @@
+(()=>{
+
+  // ここに命令を書く
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+
+  console.log('$content', $content);
+
+
+
+})();
